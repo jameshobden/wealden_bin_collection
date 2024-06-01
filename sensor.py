e0c863a8f0f71f6ef7bbaae726616b93f71990c6
James Hobden <james@jhobden.com>
2024-06-01 09:21:51 +0100

    Use a local url instead of council url

--- sensor.py
+++ sensor.py
@@ -8,7 +8,7 @@ from . import DOMAIN, CONF_UPRN
 
 _LOGGER = logging.getLogger(__name__)
 
-RESOURCE = "https://www.wealden.gov.uk/wp-admin/admin-ajax.php"
+RESOURCE = "http://MYLOCALURL.com" # Use a local proxy url to avoid scraping the council website
 
 COLLECTION_TYPES = {
     "black": "Black Bin Collection Date",
@@ -16,6 +16,9 @@ COLLECTION_TYPES = {
     "brown": "Brown Bin Collection Date"
 }
 
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+}
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the Wealden Bin Collection platform."""
@@ -59,11 +62,7 @@ class WealdenBinCollectionSensor(Entity):
 
     def update(self):
         """Fetch new state data for the sensor."""
-        payload = {
-            "action": "wealden_get_collections_for_uprn",
-            "uprn": self._uprn
-        }
-        response = requests.post(RESOURCE, data=payload)
+        response = requests.get(RESOURCE, headers=HEADERS)
 
         if response.status_code == 200:
             data = response.json()
@@ -74,17 +73,21 @@ class WealdenBinCollectionSensor(Entity):
                     if collection_date:
                         self._state = collection_date.strftime("%d/%m/%Y")
                 else:
-                    _LOGGER.error("Error retrieving collection data")
+                    _LOGGER.error("Error retrieving collection data: No collection found")
             else:
                 _LOGGER.error("Error: %s", data.get("message"))
         else:
-            _LOGGER.error("Unable to fetch data from Wealden Council API")
+            _LOGGER.error("Unable to fetch data from Wealden Council API. Status Code: %s", response.status_code)
 
     def _get_collection_date(self, collection):
         """Return the collection date based on the collection type."""
-        if self._collection_type == "black":
-            return datetime.fromisoformat(collection.get("refuseCollectionDate"))
-        if self._collection_type == "green":
-            return datetime.fromisoformat(collection.get("recyclingCollectionDate"))
-        if self._collection_type == "brown":
-            return datetime.fromisoformat(collection.get("gardenCollectionDate"))
+        try:
+            if self._collection_type == "black":
+                return datetime.fromisoformat(collection.get("refuseCollectionDate"))
+            if self._collection_type == "green":
+                return datetime.fromisoformat(collection.get("recyclingCollectionDate"))
+            if self._collection_type == "brown":
+                return datetime.fromisoformat(collection.get("gardenCollectionDate"))
+        except KeyError as e:
+            _LOGGER.error("KeyError: %s", e)
+            return None
